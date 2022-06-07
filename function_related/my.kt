bcf5edb9440ac8bf74a9fc42568d8ba48f6c38ba
Euclid <yezhizhenjiakang@gmail.com>
2022-06-07 19:45:29 +0800

    Day 2
    Back Quote for special function call;
    Cooperate with Java

--- function_related/my.kt
+++ function_related/my.kt
@@ -1,8 +1,25 @@
+//import myjava
+
+/*how to compile:
+
+kotlinc my.kt myjava.java  (.java can be omitted if we put import above)
+javac myjava.java
+
+run: 
+kotlin MyKt 
+*/
 
 fun main(){
-    who_how_old(age = 104, name = "Elaine")
+    who_how_old(age = 104, name = "Elafrine")
     who_how_old("Euclid", 26)
-    println( who_how_old("Elaine Mom") )
+    
+    println( who_how_old("Elafrine Mom") )
+    `~special weird * func`()
+ 
+    myjava.isgood()
+    
+    //myjava.is() //error. since "is" is a keyword in Kotlin but not in Java 
+    myjava.`is`() //have to use backquote
     
     TODO("Not implemented la")
     println("After TODO")
@@ -12,4 +29,7 @@ private fun who_how_old(name: String, age: Int = 22) {
     println("I, $name have age $age")
 }
 
+fun `~special weird * func`(){
+    println("I am such a weird function so weird")
+}
     
---
+++ function_related/myjava.java
@@ -0,0 +1,12 @@
+
+public class myjava{
+    
+    public static void is(){
+        System.out.println("is Invoked in Java file");
+    }
+    
+    public static void isgood(){
+        System.out.println("is Invoked in Java file");
+    }
+    
+}
