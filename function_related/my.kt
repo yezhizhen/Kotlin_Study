9941b2ccb0fbffaa02ee83e4d0e12a073ae37c4d
Euclid <yezhizhenjiakang@gmail.com>
2022-06-08 11:17:50 +0800

    Lambda String Format

--- function_related/my.kt
+++ function_related/my.kt
@@ -1,4 +1,3 @@
-//import myjava
 
 /*how to compile:
 
@@ -9,27 +8,45 @@ run:
 kotlin MyKt 
 */
 
-fun main(){
-    who_how_old(age = 104, name = "Elafrine")
-    who_how_old("Euclid", 26)
-    
-    println( who_how_old("Elafrine Mom") )
-    `~special weird * func`()
- 
-    myjava.isgood()
-    
-    //myjava.is() //error. since "is" is a keyword in Kotlin but not in Java 
-    myjava.`is`() //have to use backquote
-    
-    TODO("Not implemented la")
-    println("After TODO")
-}
-    
-private fun who_how_old(name: String, age: Int = 22) {
-    println("I, $name have age $age")
-}
+        fun named_func (p1: String, p2: Int): String{
+            return "$p1 has ${'$'} ${ if (p2 >= 0) "$p2 cash" 
+                                      else "${-p2} debt"}"
+        }
 
-fun `~special weird * func`(){
-    println("I am such a weird function so weird")
-}
-    
+        fun main(args: Array<String>){
+            who_how_old(age = 104, name = "Elafrine")
+            who_how_old("Euclid", 26)
+            
+            println( who_how_old("Elafrine Mom") )
+            `~special weird * func`()
+        
+            myjava.isgood()
+            
+            //myjava.is() //error. since "is" is a keyword in Kotlin but not in Java 
+            
+            myjava.`is`() //have to use backquote
+            
+            var credit_deficit = 0
+            
+            var lambda_func = {p1: String, p2: Int ->
+                val temp_str = if(p2 >= 0) "$p2 cash" else "${-p2} debt"
+                    "Printed by Lambda: $p1 has ${'$'} $temp_str"
+                }
+             
+            
+            println(named_func("Hi Elafrine", -40))
+            println(named_func("Hi Elafrine", 80))
+            
+            println(lambda_func("Hi Elafrine", -40))
+            println(lambda_func("Hi Elafrine", 80))
+            TODO("Not implemented la")
+            println("After TODO")
+        }
+            
+        private fun who_how_old(name: String, age: Int = 22) {
+            println("I, $name have age $age")
+        }
+        
+        fun `~special weird * func`(){
+            println("I am such a weird function so weird")
+        }
